b05952b4a149c3d9c87d9b329064d8388cf9e7c8
hardhus <ahmetcemiltoprakk@gmail.com>
2025-03-31 02:58:53 +0300

    rewrite deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-cd /home/hardhus/Desktop/WEB/battleship-ts-game/
+cd /home/hardhus/Desktop/WEB/battleship-ts-game/ || exit 1
+
+
+echo "Pulling latest changes from Git..."
+git pull
 
 echo "Installing dependencies..."
-pnpm install
+pnpm install || exit 1
 
 echo "Building the project with Vite..."
-pnpm build
+pnpm build || exit 1
 
 echo "Cleaning up old PM2 processes..."
-pm2 delete battleship --silent
+pm2 delete battleship --silent || exit 1
 
-pm2 serve dist --name "battleship" --port 4000
+echo "Starting the app with PM2..."
+pm2 serve dist --name "battleship" --port 4000 || exit 1
 
-pm2 startup
+echo "Configuring PM2 to restart on boot..."
+pm2 startup || exit 1
 
-pm2 save
+echo "Saving PM2 process list..."
+pm2 save || exit 1
 
-echo "Deployment completed!"
+echo "Deployment completed successfully!"
