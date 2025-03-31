2b49a169332e4eae95182e9a1766376666ceb257
hardhus <ahmetcemiltoprakk@gmail.com>
2025-03-31 03:56:41 +0300

    update production server url

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000/battleship");
+const socketUrl =
+    process.env.NODE_ENV === "production"
+        ? "http://battleshipserver.hardhus.com"
+        : "http://localhost:3000/battleship";
+
+const socket = io(socketUrl);
 
 export default socket;
