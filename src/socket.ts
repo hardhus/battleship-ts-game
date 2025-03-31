e2727f5acca453fef36430ee70597c5282bf779e
hardhus <ahmetcemiltoprakk@gmail.com>
2025-03-31 04:39:53 +0300

    update socket url

--- src/socket.ts
+++ src/socket.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const socketUrl =
     process.env.NODE_ENV === "production"
-        ? "https://battleshipserver.hardhus.com"
+        ? "https://battleshipserver.hardhus.com/battleship"
         : "http://localhost:3000/battleship";
 
 const socket = io(socketUrl);
